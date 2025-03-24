afb2e363efff514ef01aaf8548aaf0d6139a312d
mushfiqbh <mushfiqbh@gmail.com>
2025-03-24 14:04:26 +0600

    Fix: Image not loading on Production

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,12 @@ import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "life-insight.onrender.com",
+      },
+    ],
     domains: ["localhost", "127.0.0.1", "https://life-insight.onrender.com"],
   },
 };
